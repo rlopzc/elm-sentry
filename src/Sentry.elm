422012fbf6a26eb49e95905f9b29c86533e04b23
Romario López <romariolopezc@gmail.com>
2020-05-15 23:33:05 -0500

    Documentation

--- src/Sentry.elm
+++ src/Sentry.elm
@@ -1,21 +1,21 @@
 module Sentry exposing
-    ( Config
-    , Context
-    , Environment
-    , Level(..)
-    , ProjectId
-    , PublicKey
-    , ReleaseVersion
-    , Sentry
-    , config
-    , environment
-    , projectId
-    , publicKey
-    , releaseVersion
-    , scope
-    , send
-    , withContext
+    ( Sentry, Level(..), Config, config, Environment, environment, Context, context, ReleaseVersion, releaseVersion
+    , send, withContext
     )
+
+{-| Sentry SDK
+
+
+## Types
+
+@docs Sentry, Level, Config, config, Environment, environment, Context, context, ReleaseVersion, releaseVersion
+
+
+## Reporting errors
+
+@docs send, withContext
+
+-}
 
 import Dict exposing (Dict)
 import Http
@@ -28,25 +28,11 @@ import Time
 import UUID exposing (UUID)
 
 
+{-| Record that contains functions to report errors by Level.
 
-{-
-   https://develop.sentry.dev/sdk/event-payloads/
-      Required attributes for the Events
-       - event_id: Hexadecimal string representing a uuid4 value. The length is exactly 32 characters. Dashes are not allowed.
-       - timestamp: Unix timestamp. Time.posixToMillis // 1000
-       - logger: Name of the logger. Project name?
-       - platform: "elm"
+Create one using [`withContext`](#withContext).
 
-       -- Optionals
-       - level: fatal, error, warning, info, debug
-       - release: Version of the application. Commit SHA
-       - environment: production, staging
-       - extra: An arbitrary mapping of additional metadata to store with the event.
-       - tags:
-          - context: Can be the page name
 -}
-
-
 type alias Sentry =
     { fatal : String -> Dict String Encode.Value -> Task Http.Error UUID
     , error : String -> Dict String Encode.Value -> Task Http.Error UUID
@@ -56,6 +42,8 @@ type alias Sentry =
     }
 
 
+{-| The event severity.
+-}
 type Level
     = Fatal
     | Error
@@ -83,37 +71,60 @@ levelToString level =
             "debug"
 
 
+{-| The environment name, such as "production" or "staging".
+
+Create one using [`environment`](#environment).
+
+-}
 type Environment
     = Environment String
 
 
+{-| Create an [`Environment`](#Environment).
+
+    Sentry.environment "production"
+
+-}
 environment : String -> Environment
 environment =
     Environment
 
 
+{-| The release version of the application.
+
+This value can be a git SHA, or a product identifier with a semantic version.
+
+Create one using [`releaseVersion`](#releaseVersion).
+
+-}
 type ReleaseVersion
     = ReleaseVersion String
 
 
+{-| Create a [`ReleaseVersion`](#ReleaseVersion).
+
+    Sentry.releaseVersion "721e41770371db95eee98ca2707686226b993eda"
+
+-}
 releaseVersion : String -> ReleaseVersion
 releaseVersion =
     ReleaseVersion
 
 
-{-| Context is going to be sent as Tag
-<https://docs.sentry.io/enriching-error-data/additional-data/>
-tags: {
-context: ""
-}
+{-| The Context, for example, the Page name.
 
-That's why the scoped fn is useful. You define the scope in the page and use that to sent logs
+Crate one using [`context`](#context).
 
 -}
 type Context
     = Context String
 
 
+{-| Create a [`Context`](#Context).
+
+    Sentry.context "profile/settings"
+
+-}
 context : String -> Context
 context =
     Context
@@ -127,23 +138,78 @@ type PublicKey
     = PublicKey String
 
 
+type Host
+    = Host String
+
+
 type ProjectId
     = ProjectId String
 
 
+{-| The configuration of the SDK.
+
+Specifies the Public Key, the Host, and the Project ID.
+
+Create one using [`config`](#config).
+
+-}
 type Config
-    = Config PublicKey ProjectId
+    = Config PublicKey Host ProjectId
 
 
+{-| Creates a [`Config`](#Config).
+
+Use the dsn to provide the publicKey, host and projectId.
+
+    -- Example: https://1900942c246350fdacb4c9369cac2ets@o298593.ingest.sentry.io/2312456
+    Sentry.config
+        { publicKey = "1900942c246350fdacb4c9369cac2ets"
+        , host = "o298593.ingest.sentry.io"
+        , projectId = "2312456"
+        }
+
+-}
 config :
     { publicKey : String
+    , host : String
     , projectId : String
     }
     -> Config
 config conf =
-    Config (PublicKey conf.publicKey) (ProjectId conf.projectId)
+    Config (PublicKey conf.publicKey) (Host conf.host) (ProjectId conf.projectId)
 
 
+{-| Build a [`Sentry`](#Sentry) record configured with the given
+[`Config`](#Config), [`ReleaseVersion`](#ReleaseVersion), an
+[`Environment`](#Environment) and a [`Context`](#Context) string.
+
+        import Sentry exposing (Sentry)
+
+        config : Sentry.Config
+        config =
+            Sentry.config
+                { publicKey = "1900942c246350fdacb4c9369cac2ets"
+                , host = "o298593.ingest.sentry.io"
+                , projectId = "2312456"
+                }
+
+        releaseVersion : Sentry.ReleaseVersion
+        releaseVersion =
+            Sentry.releaseVersion "721e41770371db95eee98ca2707686226b993eda"
+
+        environment : Sentry.Environment
+        environment =
+            Sentry.environment "production"
+
+        sentry : Sentry
+        sentry =
+            Sentry.withContext config releaseVersion environment "profile/settings"
+
+        sentry.fatal "Backend on fire!" (Dict.fromList [("response", toString response)])
+        sentry.debug "Testing sentry integration" Dict.empty
+        sentry.info "Backend timeout when saving the profile settings" Dict.empty
+
+-}
 withContext : Config -> ReleaseVersion -> Environment -> String -> Sentry
 withContext vconfig vreleaseVersion venvironment contextStr =
     let
@@ -158,6 +224,29 @@ withContext vconfig vreleaseVersion venvironment contextStr =
     }
 
 
+{-| Send an error to Sentry.
+
+Normally, it's preferable to use [`withContext`](#withContext)
+as it provides a reusable configured [`Sentry`](#Sentry).
+
+Arguments:
+
+  - [`Config`](#Config): The configuration about your Sentry team and project. Create one using [`config`](#config)
+  - [`Level`](#Level): The severity of the error, e.g. `Fatal`, `Error`, `Info`
+  - [`ReleaseVersion`](#ReleaseVersion): Your app version. Can be a git commit SHA.
+  - [`Environment`](#Environment): e.g. `"production"`, `"staging"`
+  - [`Context`](#Context): Scopes the message. It can be the name of the Page that produced this error, e.g. `"profile/settings"`
+  - `String`: The error message, e.g. `"Backend on fire!"`
+  - `Dict String Value`: Extra information, e.g. `{"response": "503 Service unavailable"}`
+
+On success, the [`Task`](http://package.elm-lang.org/packages/elm-lang/core/latest/Task#Task)
+will resolve to a [`UUID`](https://package.elm-lang.org/packages/TSFoster/elm-uuid/latest/UUID#UUID)
+that was sent to Sentry as the Event ID.
+
+On failure, the [`Http.Error`](http://package.elm-lang.org/packages/elm-lang/http/latest/Http#Error) will
+hold the information about the problem.
+
+-}
 send : Config -> Level -> ReleaseVersion -> Environment -> Context -> String -> Dict String Encode.Value -> Task Http.Error UUID
 send vconfig level vreleaseVersion venvironment vcontext message metadata =
     Time.now
@@ -165,7 +254,7 @@ send vconfig level vreleaseVersion venvironment vcontext message metadata =
 
 
 sendWithTime : Config -> Level -> ReleaseVersion -> Environment -> Context -> String -> Dict String Encode.Value -> Time.Posix -> Task Http.Error UUID
-sendWithTime (Config vpublicKey vprojectId) level vreleaseVersion venvironment vcontext message metadata posix =
+sendWithTime (Config vpublicKey vhost vprojectId) level vreleaseVersion venvironment vcontext message metadata posix =
     let
         uuid : UUID
         uuid =
@@ -180,7 +269,7 @@ sendWithTime (Config vpublicKey vprojectId) level vreleaseVersion venvironment v
     in
     { method = "POST"
     , headers = [ authHeader posix vpublicKey ]
-    , url = endpointUrl vprojectId
+    , url = endpointUrl vhost vprojectId
     , body = body
     , resolver = Http.stringResolver (\_ -> Ok ())
     , timeout = Nothing
@@ -215,9 +304,11 @@ withRetry maxRetryAttempts task =
     Task.onError retry task
 
 
+{-| <https://develop.sentry.dev/sdk/event-payloads/>
+-}
 toJsonBody : UUID -> Time.Posix -> Level -> ReleaseVersion -> Environment -> Context -> String -> Dict String Encode.Value -> Http.Body
 toJsonBody uuid posix level (ReleaseVersion vreleaseVersion) (Environment venvironment) (Context vcontext) message metadata =
-    [ ( "event_id", Encode.string (uuidRemoveDashes uuid) )
+    [ ( "event_id", Encode.string (UUID.toRepresentation UUID.Compact uuid) )
     , ( "timestamp", Encode.int (posixToSeconds posix) )
     , ( "plaform", Encode.string "elm" )
     , ( "level", Encode.string (levelToString level) )
@@ -232,11 +323,6 @@ toJsonBody uuid posix level (ReleaseVersion vreleaseVersion) (Environment venvir
     ]
         |> Encode.object
         |> Http.jsonBody
-
-
-uuidRemoveDashes : UUID -> String
-uuidRemoveDashes =
-    UUID.toString >> String.replace "-" ""
 
 
 
@@ -255,16 +341,15 @@ messageEncoder vmessage =
         ]
 
 
+{-| <https://develop.sentry.dev/sdk/overview/#authentication>
 
-{- https://develop.sentry.dev/sdk/overview/#authentication
-   X-Sentry-Auth: Sentry sentry_version=7,
-     sentry_client=<client version, arbitrary>, version of this client: 0.0.1
-     sentry_timestamp=<current timestamp>,
-     sentry_key=<public api key>,
-     sentry_secret=<secret api key>
+    X-Sentry-Auth: Sentry sentry_version=7,
+    sentry_client=<client version, arbitrary>,
+    sentry_timestamp=<current timestamp>,
+    sentry_key=<public api key>,
+    sentry_secret=<secret api key>
+
 -}
-
-
 authHeader : Time.Posix -> PublicKey -> Http.Header
 authHeader posix (PublicKey vpublicKey) =
     Http.header "X-Sentry-Auth" <|
@@ -279,9 +364,11 @@ sentryVersion =
     "7"
 
 
-endpointUrl : ProjectId -> String
-endpointUrl (ProjectId vprojectId) =
-    "https://sentry.com/api/" ++ vprojectId ++ "/store/"
+{-| <https://develop.sentry.dev/sdk/overview/#parsing-the-dsn>
+-}
+endpointUrl : Host -> ProjectId -> String
+endpointUrl (Host vhost) (ProjectId vprojectId) =
+    "https://" ++ vhost ++ "/api/" ++ vprojectId ++ "/store/"
 
 
 retries : { maxAttempts : Int, msDelayBetweenRetries : Float }
@@ -291,7 +378,7 @@ retries =
     }
 
 
-{-| Turn a Posix.Time into the number of seconds since Epoch
+{-| Turn a Posix.Time into the number of seconds since Epoch.
 -}
 posixToSeconds : Time.Posix -> Int
 posixToSeconds posix =
